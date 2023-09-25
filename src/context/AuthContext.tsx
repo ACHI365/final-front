188c868d1a5d40b75a41b8c5081c88e5932f6b17
ACHI365 <archili.melikidze2003@gmail.com>
2023-09-25 16:33:06 +0400

    modified:   src/context/AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -31,25 +31,48 @@ export const useAuth = (): AuthContextType => {
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
-    localStorage.getItem("jwtToken") !== null
+    // localStorage.getItem("jwtToken") !== null
+    document.cookie.includes("jwtToken")
+
   );
   const [currUser, setCurrUser] = useState<User>();
 
-  const login = (data: any): void => {
-    localStorage.setItem("jwtToken", data.token);
+  // const login = (data: any): void => {
+  //   localStorage.setItem("jwtToken", data.token);
     
+  //   let loggedUser = data.result.data;
+  //   console.log(loggedUser);
+  //   setCurrUser(loggedUser)
+  //   localStorage.setItem("userID", String(loggedUser?.userId))
+  //   localStorage.setItem("userRole", String(loggedUser?.role))
+  //   setIsAuthenticated(true);
+  // };
+
+  // const logout = (): void => {
+  //   localStorage.removeItem("jwtToken");
+  //   localStorage.removeItem("userID");
+  //   localStorage.removeItem("userRole");
+  //   setIsAuthenticated(false);
+  // };
+
+  const login = (data: any): void => {
+    document.cookie = `jwtToken=${data.token}; path=/`;
+
     let loggedUser = data.result.data;
     console.log(loggedUser);
-    setCurrUser(loggedUser)
-    localStorage.setItem("userID", String(loggedUser?.userId))
-    localStorage.setItem("userRole", String(loggedUser?.role))
+    setCurrUser(loggedUser);
+
+    document.cookie = `userID=${loggedUser?.userId}; path=/`;
+    document.cookie = `userRole=${loggedUser?.role}; path=/`;
+
     setIsAuthenticated(true);
   };
 
   const logout = (): void => {
-    localStorage.removeItem("jwtToken");
-    localStorage.removeItem("userID");
-    localStorage.removeItem("userRole");
+    document.cookie = "jwtToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+    document.cookie = "userID=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+    document.cookie = "userRole=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/";
+
     setIsAuthenticated(false);
   };
 
